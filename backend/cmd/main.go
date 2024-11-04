b061b2f6c6a6fa208f3e73c4942665b71fcf5ffd
mergen <tiuleev@sfedu.ru>
2024-11-05 02:11:08 +0300

    deps on check 17

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,12 +1,6 @@
 package main
 
 import (
-	"apiapp/internal/handlers"
-	"apiapp/internal/repository"
-	"database/sql"
-	"fmt"
-	"log"
-	"net/http"
 	"os"
 
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
@@ -16,21 +10,5 @@ var secretKey = os.Getenv("secretKey")
 
 
 func main() {
-	url := os.Getenv("TOKEN")
-	if url==""{
-		panic("uykuykyyukukkukujhtg")
-	}
-	log.Print(url)
-	db, err := sql.Open("libsql", url)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to open db %s: %s", url, err)
-		os.Exit(1)
-	}
-	defer db.Close()
-	
-	
-	repo := repository.NewUserRepo(db)
-	h := handlers.NewHandler("ccc", repo)
-	r := h.InitRoutes()
-	http.ListenAndServe(":8080", r)
+
 }
